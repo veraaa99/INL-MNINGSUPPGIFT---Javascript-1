6e5006fff189e3bc2e66ac0491b2095122cf92c9
Vera <althinivera77@gmail.com>
2024-11-20 16:12:44 +0100

    Removed comments containing the instructions for the assignment.

--- js/Inlämningsuppgift.js
+++ js/Inlämningsuppgift.js
@@ -1,8 +1,3 @@
-// Krav för godkänt:
-
-// Hämta todos 
-// Använd Fetch för att göra en GET-förfrågan och hämta Todo-listan från API:et vid sidans laddning. Presentera listan på din webbsida.
-
 /*
 * VARIABLES 
 */
@@ -99,14 +94,6 @@ const showTodos = () => {
     })
 }
 
-// Lägga till todos 
-// Skapa ett formulär med en textinput och en knapp, som låter användaren lägga till en ny Todo. 
-// Validera inmatningen för att undvika att man kan lägga till tomma Todos. Försöker man lägga till en todo utan att ha fyllt i någon text så ska ett felmeddelande visas för användaren.
-
-// Spara todo till databasen 
-// Använd Fetch för att göra en POST-förfrågan till API:et när en ny todo läggs till. 
-// Uppdatera listan på hemsidan med den nya todon efter att du har fått ett svar från API:et.
-
 /*
 * SUBMIT NEW TODO
 */
@@ -168,13 +155,6 @@ const createTodo = async() => {
 
     return;
 }
-
-// Ta bort todo 
-// Implementera funktionen att ta bort en todo från listan genom att göra en DELETE-förfrågan till API:et. 
-// Uppdatera DOM:en efter du har fått ett svar tillbaka så att den borttagna todon försvinner.
-
-// Din API-nyckel:
-// 6e308da8-8ca1-488a-99f5-fc410760a050
 
 /*
 * REMOVE TODO
@@ -240,19 +220,6 @@ const removeTodo = async(todo) => {
 
     return true
 }
-
-// Klarmarkera todos 
-// Låt användarna klarmarkera en todo genom att skicka en PUT-förfrågan till API:et. 
-// Uppdatera stylingen på den klarmarkerade todon för att visa tydligt att den är avklarad. 
-// Möjliggör även att ändra tillbaka statusen till "ej avklarad" om det behövs.
-
-// Visuell indikation för klarmarkering 
-// Todos som är klarmarkerade när de hämtas från databasen ska presenteras med samma styling som ovan som indikerar deras avklarade status.
-
-// Förhindra borttagning av ej klarmarkerade todos 
-// Förhindra användare från att ta bort todos som inte är klarmarkerade. 
-// Om användaren ändå försöker ta bort en sådan todo ska en modal (popup) visas istället med en text som förklarar varför borttagningen inte är tillåten. 
-// Denna får inte vara en vanlig alert()
 
 /*
 * UPDATE TODO
